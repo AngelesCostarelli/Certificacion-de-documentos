76214068ddcdc4a61d2def5f23d193841d3a2d43
AngelesCostarelli <costarelli.angeles@gmail.com>
2022-04-12 14:38:19 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 const AUTH0_DOMAIN = "https://extrimian.us.auth0.com";
-const AUTH0_CLIENT_ID = "aMC7o14a3Rt3jiD5s0RpQJGwhxbjywlV";
+const AUTH0_CLIENT_ID = "P3n4NXvogCOQydAdc4f3qJLi8ycKvLqv";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
